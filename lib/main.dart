3628eb4ef70430d620aa43f0b29c3f6e309999c2
ecenurkarahan <ekarahan21@ku.edu.tr>
2024-08-16 16:01:56 +0300

    Homework 1 is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -13,40 +15,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -86,23 +64,15 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/pink_marble.jpg'),
+          fit: BoxFit.cover
+        ),
+      ),
+        child : Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -114,12 +84,93 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+class LoginPage extends StatefulWidget{
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState()=> _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage>{
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  //this will be updated later
+  void _login() {
+    final String username = _usernameController.text;
+    final String password = _passwordController.text;
+    if(username.isEmpty|| password.isEmpty){
+      Fluttertoast.showToast(msg: "Please fill empty fields!",
+      toastLength: Toast.LENGTH_SHORT,
+      fontSize: 18.0,
+      gravity: ToastGravity.CENTER);
+    }
+    else if(username=="Jhon Doe"&& password=="12345678"){
+      Fluttertoast.showToast(msg: "Login Successful!",
+          toastLength: Toast.LENGTH_SHORT,
+          fontSize: 18.0,
+          gravity: ToastGravity.CENTER);
+    }
+    else{
+      Fluttertoast.showToast(msg: "Your credentials are wrong!",
+          toastLength: Toast.LENGTH_SHORT,
+          fontSize: 18.0,
+          gravity: ToastGravity.CENTER);
+    }
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage( 'assets/images/pink_marble.jpg'),
+            fit: BoxFit.cover
+            ),
+      ),
+        child: Center(
+          child: Padding(
+            padding:const EdgeInsets.all(16.0),
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    filled:true,
+                    fillColor: Colors.white70
+                  ),
+                  style: const TextStyle(fontFamily: 'CustomFont'),
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    filled: true,
+                    fillColor: Colors.white70
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(onPressed: _login,
+                    child: const Text('Login'),
+                )
+              ],
+            )
+          )
+        ),
+    )
     );
   }
 }
